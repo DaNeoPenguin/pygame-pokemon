16c97a476bf6db835084cb5439bc56dcd98af020
titoudelaplage <totolegamerofficiel@gmail.com>
2021-06-01 18:06:01 +0200

    added map + some assets

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import pygame
+pygame.init()
+# win.create
+pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Pokémon en python")
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# boucle
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.QUIT()
